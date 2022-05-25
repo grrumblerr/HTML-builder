5928c078e2ce8c144ea654936848b0f789f5efe7
grrumblerr <grrumblerr@yandex.by>
2022-05-25 16:59:16 +0300

    task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,34 +1,60 @@
-
 const fs = require('fs');
 const path = require('path');
-const dir = path.join(__dirname, 'files');
+
+const source = path.join(__dirname, 'files');
 const dest = path.join(__dirname, 'files-copy');
 
-fs.mkdir(dest, { recursive: true }, err => {
-  if (err) throw err;
-});
+async function copyDir(src, res) {
+  await fs.promises.rm(res, { recursive: true, force: true });
+  await fs.promises.mkdir(res);
 
-fs.readdir(dest, (err, file) => {
-  if (err) throw err;
-  for (let i of file) {
-    fs.unlink(path.join(dest, i), err => {
+  let files = await fs.promises.readdir(src);
+
+   
+  for (const file of files) {
+    fs.copyFile(path.join(src, file), path.join(res, file), err => {
       if (err) throw err;
+      console.log(`File ${file} is copied`);
     });
   }
-});
+}
+
+copyDir(source, dest);
 
 
-fs.readdir(dir, function(err, files) {
-  if (err) throw err;
 
-  files.forEach(function (name) {
-    let input = path.join(dir, name);
-    let output = path.join(dest, name);
 
-    fs.copyFile(input, output, err => {
-      if (err) throw err;
-      console.log(`File ${name} is copied`);
-    });
 
-  });
-});
+// const fs = require('fs');
+// const path = require('path');
+// const dir = path.join(__dirname, 'files');
+// const dest = path.join(__dirname, 'files-copy');
+
+// fs.mkdir(dest, { recursive: true }, err => {
+//   if (err) throw err;
+// });
+
+// fs.readdir(dest, (err, file) => {
+//   if (err) throw err;
+//   for (let i of file) {
+//     fs.unlink(path.join(dest, i), err => {
+//       if (err) throw err;
+//     });
+//   }
+// });
+
+
+// fs.readdir(dir, function(err, files) {
+//   if (err) throw err;
+
+//   files.forEach(function (name) {
+//     let input = path.join(dir, name);
+//     let output = path.join(dest, name);
+
+//     fs.copyFile(input, output, err => {
+//       if (err) throw err;
+//       console.log(`File ${name} is copied`);
+//     });
+
+//   });
+// });
